9abe3a14d23feb012dc180be316c172c398a0938
hansotronic <serviceshansen@gmail.com>
2013-04-15 09:20:59 +0200

    fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "virtualbox"
 maintainer       "Chris Peplin"
 maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
@@ -5,6 +6,6 @@ description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.7.2"
 
-%w{ubuntu mac_os_x windows}.each do |os|
+%w{ubuntu mac_os_x windows centos}.each do |os|
   supports os
 end
